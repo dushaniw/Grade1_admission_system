3464c3d24202a400c100b5902aad9771de405712
Nuwantha Dewappriya <nuwanthadew@gmail.com>
2015-12-17 08:41:29 +0530

    signup working

--- grade1_admission/app/controllers/GuardianController.php
+++ grade1_admission/app/controllers/GuardianController.php
@@ -13,19 +13,18 @@ class GuardianController extends BaseController
         $last_name = Input::get("RESULT_TextField-2");
         $email = Input::get("RESULT_TextField-3");
         $password = Input::get("RESULT_TextField-4");
-        $conform_password = Input::get("RESULT_TextField-5");
+        //$conform_password = Input::get("RESULT_TextField-5");
         $nic = Input::get("RESULT_TextField-6");
         $telephone_number =Input::get("RESULT_TextField-7");
         $relation_to_child=Input::get("RESULT_RadioButton-8");
         $religion=Input::get("RESULT_TextField-9");
         $nationality = Input::get("RESULT_TextField-10");
         $grama_niladry_division_number = Input::get("RESULT_TextField-11");
-        $district = Input::get("RESULT_TextField-12");
-        $permanent_address = Input::get("RESULT_RadioButton-13");
+        //$district = Input::get("RESULT_RadioButton-12");
+        $permanent_address = Input::get("RESULT_TextArea-13");
 
 
-        DB::Insert("insert into gurdian values(?,?,?,?,?,?,?,?,?)",array($nic,$first_name,$last_name,$relation_to_child,$nationality,$religion,$permanent_address,$email,$telephone_number,$grama_niladry_division_number,$password ));
-       // return View:: make('Welcome/submitted')->with('var',$full_name);
+        DB::Insert("insert into guardian values(?,?,?,?,?,?,?,?,?,?,?)",array($nic,$first_name,$last_name,$relation_to_child,$nationality,$religion,$permanent_address,$email,$telephone_number,$grama_niladry_division_number,$password ));
         return Redirect ::to('/SignUpGuardian');
 
     }
