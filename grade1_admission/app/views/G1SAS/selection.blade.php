cdde0a9277b8b9e3b97c3730bd5978df6d12d4b3
Gimhani <gimhanieuthpala@gmail.com>
2016-01-08 21:22:26 +0530

    did few edit

---
+++ grade1_admission/app/views/G1SAS/selection.blade.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+	<title> Select School</title>
+</head>
+<body>
+
+	{{ Form :: open(array('url' =>'selectedSuccessfully','method' => 'POST'))}}
+
+<!--,'action' => 'SchoolDBController@getSchoolList'-->
+
+	{{Form::label('selectlabel', 'Select your six schools    ');}}
+
+	<br>
+
+	<div class="form-school-group">
+ 		 {{Form::label('school1label', 'School 1    ');}}
+  		<select class="form-school-control" name="school_name">
+    		@foreach($schools as $school)
+      			<option value="{{$school->getSchool_id()}}">{{$school->getSchool_name()}}</option>
+    		@endforeach
+  		</select>
+	</div>
+<br>
+	<div class="form-school-group">
+ 		 	{{Form::label('school2label', 'School 2    ');}} 
+  		<select class="form-school-control" name="school_name">
+    		@foreach($schools as $school)
+      			<option value="{{$school->getSchool_id()}}">{{$school->getSchool_name()}}</option>
+    		@endforeach
+  		</select>
+	</div>
+<br>
+    
+	<div class="form-school-group">
+ 			{{Form::label('school3label', 'School 3    ');}}   
+  		<select class="form-school-control" name="school_name">
+    		@foreach($schools as $school)
+      			<option value="{{$school->getSchool_id()}}">{{$school->getSchool_name()}}</option>
+    		@endforeach
+  		</select>
+	</div>
+
+<br>
+	<div class="form-school-group">
+		   	{{Form::label('school4label', 'School 4    ');}}
+  		<select class="form-school-control" name="school_name">
+    		@foreach($schools as $school)
+      			<option value="{{$school->getSchool_id()}}">{{$school->getSchool_name()}}</option>
+    		@endforeach
+  		</select>
+	</div>
+
+<br>
+	<div class="form-school-group">
+		   {{Form::label('school5label', 'School 5    ');}}
+  		<select class="form-school-control" name="school_name">
+    		@foreach($schools as $school)
+      			<option value="{{$school->getSchool_id()}}">{{$school->getSchool_name()}}</option>
+    		@endforeach
+  		</select>
+	</div>
+
+<br>
+   	<div class="form-school-group">
+		       {{Form::label('school6label', 'School 6    ');}}
+  		<select class="form-school-control" name="school_name">
+    		@foreach($schools as $school)
+      			<option value="{{$school->getSchool_id()}}">{{$school->getSchool_name()}}</option>
+    		@endforeach
+  		</select>
+	</div>
+
+<br>
+      {{Form::submit('Add Application');}}
+      {{ Form:: close()}}
+
+    
+  
+</body>
+</html>
