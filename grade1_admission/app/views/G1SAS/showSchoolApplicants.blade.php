584937fe9b044e511fe356e36bd7c145e6a4ff28
Nuwantha Dewappriya <nuwanthadew@gmail.com>
2016-01-15 12:35:50 +0530

    table shows beautifully

--- grade1_admission/app/views/G1SAS/showSchoolApplicants.blade.php
+++ grade1_admission/app/views/G1SAS/showSchoolApplicants.blade.php
@@ -1,6 +1,25 @@
 <html>
 <head>
 	<title>Selected Applicants </title>
+	<style>
+	table{
+		border-collapse: collapse;
+		border-spacing: 10px;
+	}
+	th{
+		background-color: #4CAF50;
+    	color: white;
+	}
+	table,th,td{
+		width:100% ;
+		text-align: left;
+		height: 30px;
+		padding: 15px;
+		border-bottom: 1px solid #ddd;
+    	
+	}
+	tr:hover{background-color:#f5f5f5}
+	</style>
 </head>
 
 <body>
@@ -11,12 +30,25 @@
 		{{Form::label('title','Selected Applicant List for:	')}}
 		{{Form::text('school_name',$selected)}}
 	<br>
-		
-		<table>
-	    @foreach($applicants as $applicant)
-	        <tr><td>{{$applicant->getApplicantId()}} - {{$applicant->getFirstname()}} - {{$applicant->getLastname()}}</td></tr>
-	    @endforeach
+		<div style="overflow-x:auto;">
+		<table >
+			<tr>
+	    		<th>Application ID</th>
+	    		<th>Frist Name</th> 
+	    		<th>Last Name</th>
+	    		<th>Gender</th>
+	    		<th>DateOfBirth</th>
+  			</tr>
+	    	@foreach($applicants as $applicant)
+	        <tr>
+	        	<td>{{$applicant->getApplicantId()}}</td>
+	        	<td>{{$applicant->getFirstname()}}</td>
+	         	<td>{{$applicant->getLastname()}}</td>
+	         	<td>{{$applicant->getGender()}}</td>
+	         	<td>{{$applicant->getDateOfBirth()}}</td>
+	         </tr>
+	    	@endforeach
 		</table>
-	
+		</div>
 </body>
 </html>
