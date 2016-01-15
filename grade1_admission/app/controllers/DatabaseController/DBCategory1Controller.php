0df2e16e2e35feaaac03ed114c5d617969f01c51
Gimhani <gimhanieuthpala@gmail.com>
2016-01-15 15:08:08 +0530

    add dbcategory1Controller

---
+++ grade1_admission/app/controllers/DatabaseController/DBCategory1Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class DBCategory1Controller{
+
+	public static function addCategory1($category){
+		$db=Connection::getInstance();
+        $mysqli=$db->getConnection();
+       	$query="insert into residentInClosedProximity values( '$guardianNic','$category->getNoOfYearsInElectrocalRegister()','$category-> getNoOfYearsSpouseInElectrocalRegister()','$category->getTypeOfTitleDeed()','$category->getTotalServicePeriod()','$category->getNoOfAditionalDocumentForResident()')";
+       	return $mysqli->query($query);
+	}
+}
