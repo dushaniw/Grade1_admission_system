1687d5bd235cc4ecea3cec2dc93bee4cc2653b29
Nuwantha Dewappriya <nuwanthadew@gmail.com>
2016-01-15 17:39:29 +0530

    some unwanted files deleted and some renamed

--- grade1_admission/app/views/G1SAS/AddNewChild.blade.php
+++ grade1_admission/app/views/G1SAS/AddNewChild.blade.php
@@ -5,9 +5,7 @@
 <body>
 
 {{ Form :: open(array('url' =>'child/add','method' => 'POST' ))}}
-        
-
-{{$username}} 
+  
         {{Form::label('usernameLabel', 'Username:      ');}}
         {{ Form :: text( 'usernameText',$username) ; }}     
         <br>
