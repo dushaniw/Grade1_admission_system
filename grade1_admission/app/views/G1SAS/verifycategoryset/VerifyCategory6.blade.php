c54170ed92709d3aac390b16688d2021fd14888d
Dushani <dushaniwellappili@gmail.com>
2016-01-19 23:06:34 +0530

    cat 5 n 6 done

--- grade1_admission/app/views/G1SAS/verifycategoryset/VerifyCategory6.blade.php
+++ grade1_admission/app/views/G1SAS/verifycategoryset/VerifyCategory6.blade.php
@@ -1,106 +1,134 @@
-<html>
-<head>
-	<title>Verify Category 1</title>
-</head>
-<body>
-	{{ Form :: open(array('url' =>'school/verifycat','method' => 'POST' ))}}
-		
-		{{Form :: label( 'schoolid','School id:') ; }}
-		{{Form :: text( 'schoolIdText',$school->getSchool_id(), ['readonly']) ; }}
-		{{Form :: label( 'schoolName','My School :') ; }}
-		{{Form :: text( 'schoolNameText',$school->getSchool_name(), ['readonly']) ; }}
-		<br>
+@extends('G1SAS/layouts/normal_user_layout')
+@section('content')
 
-		{{Form::label('application_id','Application ID:')}}
-		{{Form::text('application_idtext',$application_id, ['readonly'])}}
-		<br>
+<nav class = "navbar navbar-inverse" role = "navigation">
+                <ul class="nav navbar-nav" >
+                <li> <a href = "/school/overview?schoolidtext=<?php echo $school->getSchool_id()?>">Home</a></li>
+                <li>{{HTML::link('#', 'About')}}        </li>
+                <li class><a href = "/school/viewpastpupilmarkingcriteria?schoolid=<?php echo $school->getSchool_id()?>"> Past Pupil </a>  </li>
+                <li class="active"><a href = "/school/verifyapplication?schoolid=<?php echo $school->getSchool_id()?>">Applications</a> </li>
+        </ul>
+                <p class = "navbar-text pull-right">
+         Signed in as <a href = "#" class = "navbar-link"><?php echo $school->getEmail()?></a> | <a href = "#" class = "navbar-link">Sign Out</a> </p>
+        </nav>
+        <div class="row">
+        	<div class="col-md-3">
+            	    <br/>
+                	<ul class = "nav nav-pills nav-stacked" role = "navigation">
+                        <li class="active"><a href = "/school/verifyapplication?schoolid=<?php echo $school->getSchool_id()?>">Verify Applications</a></li>
+                        <li ><a href = "/search/schoolselected">Search Selected Applicants</a>
+						</li>
+                	</ul>
+        	</div>
+        	<div class = "col-md-9">
+        	    <div class = "panel panel-primary panel-transparent">
+                    <div class ="panel-heading">
+                        <h3 class="panel-title"> <p class="text-center"><span class = "glyphicon glyphicon-check"></span> Verify Application : <?php echo $application_id?></p></h3>
+                    </div>
+                    <div class = "panel-body"> <div class = "col-md-10">
+						{{ Form:: open(array('url' =>'school/verifycat','method' => 'POST','role'=>'form' ))}}
+						<div class="form-group">
+							{{Form:: label( 'schoolid','School ID') }}
+							{{Form:: text( 'schoolIdText',$school->getSchool_id(),array('class' =>'form-control','style'=>'width: 100px; background-color:white','readonly') )}}
+						</div>
+						<div class="form-group">
+							{{Form:: label( 'schoolName','School Name') }}
+							{{Form:: text( 'schoolNameText',$school->getSchool_name(),array('class' =>'form-control','style'=>'background-color:white','readonly')) }}
+						</div>
+						<div class="form-group">
+							{{Form::label('application_id','Application ID')}}
+							{{Form::text('application_idtext',$application_id, array('class' =>'form-control','style'=>'width: 100px; background-color:white','readonly'))}}
+						</div>
+						<div class="form-group">
+							{{Form:: label( 'applicant_name','Student Applicant Name') }}
+							{{Form:: text( 'applicant_nametext',$applicant->getFirstname()." ".$applicant->getLastname(), array('class' =>'form-control','style'=>'background-color:white','readonly')) }}
+						</div>
+						<div class="form-group">
+							{{Form:: label( 'guardian_name','Guardian Name')  }}
+							{{Form:: text( 'guardian_nametext',$guardian->getFirstName()." ".$guardian->getLastName(),array('class' =>'form-control','style'=>'background-color:white','readonly'))  }}
+						</div>
+						<div class="form-group">
+							{{Form:: label( 'guardian_nic','Guardian NIC')  }}
+							{{Form:: text( 'guardian_nictext',$guardian->getNic(),array('class' =>'form-control','style'=>'width: 150px; background-color:white','readonly') ) }}
+						</div>
+						<div class="form-group">
+							{{Form:: label( 'gender','Gender')  }}
+							{{Form:: text( 'gendertext',$applicant->getGender(),array('class' =>'form-control','style'=>'width: 150px; background-color:white','readonly') ) }}
+						</div>
+						<div class="form-group">
+							{{Form:: label( 'religion','Religion') }}
+							{{Form:: text( 'religiontext',$applicant->getReligion(),array('class' =>'form-control','style'=>'width: 150px; background-color:white','readonly') ) }}
+						</div>
+						<div class="form-group">
+							{{Form:: label( 'dob','Date of Birth')  }}
+							{{Form:: text( 'dobtext',$applicant->getDateOfBirth(), array('class' =>'form-control','style'=>'width: 150px; background-color:white','readonly') ) }}
+						</div>
+						<div class="form-group">
+							{{Form:: label( 'orderOfPreference','This school is applied as')  }}
+							{{Form:: text( 'orderOfPreferencetext',$application->getOrderOfPreference(), array('class' =>'form-control','style'=> 'width: 100px; background-color:white','readonly') ) }}
+						</div>
+						<div class="form-group">
+							{{Form:: label( 'typeOfApplication','Applied under category:') ; }}
+							@if($application->getType()==1)
+								{{Form:: text( 'typeOfApplicationtext','Resident in closed proximity', array('class' =>'form-control','style'=>'width: 400px; background-color:white','readonly') ) }}
+							@elseif($application->getType()==2)
+								{{Form:: text( 'typeOfApplicationtext','Past Pupil of the School',array('class' =>'form-control','style'=>'width: 400px; background-color:white','readonly') ) }}
+							@elseif($application->getType()==3)
+								{{Form:: text( 'typeOfApplicationtext','A sibling is learing currenly in school',array('class' =>'form-control','style'=>'width: 400px; background-color:white','readonly') ) }}
+							@elseif($application->getType()==4)
+								{{Form:: text( 'typeOfApplicationtext','Educational officer',array('class' =>'form-control','style'=>'width: 400px; background-color:white','readonly') )}}
+							@elseif($application->getType()==5)
+								{{Form:: text( 'typeOfApplicationtext','An officer on transfer', array('class' =>'form-control','style'=>'width: 400px; background-color:white','readonly'))}}
+							@elseif($application->getType()==6)
+								{{Form:: text( 'typeOfApplicationtext','A person coming from abroad', array('class' =>'form-control','style'=>'width: 400px; background-color:white','readonly')) }}
+							@endif
+						</div>
+						<div class="form-group">
+							{{Form:: label( 'distance','Distance to school')  }} 
+							{{Form:: text( 'distancetext',$application->getDistance()." km", array('class' =>'form-control','style'=>'width: 150px; background-color:white','readonly')) }} 
+						</div>
+						<div class="form-group">
+							{{Form:: label( 'medium','Requested Learning Medium')  }}
+							{{Form:: text( 'mediumtext',$application->getMedium(), array('class' =>'form-control','style'=>'width: 150px; background-color:white','readonly')) }}
+						</div>
 
-		<br>
-		{{Form :: label( 'applicant_name','Student Applicant Name:') ; }}
-		{{Form :: text( 'applicant_nametext',$applicant->getFirstname()." ".$applicant->getLastname(), ['readonly']); }}
-		<br>
-		{{Form :: label( 'guardian_name','Guardian Name:') ; }}
-		{{Form :: text( 'guardian_nametext',$guardian->getFirstName()." ".$guardian->getLastName(), ['readonly']); }}
-		<br>
-		{{Form :: label( 'guardian_nic','Guardian NIC:') ; }}
-		{{Form :: text( 'guardian_nictext',$guardian->getNic(), ['readonly']); }}
-		<br>
-		<br>
-		{{Form :: label( 'gender','Gender:') ; }}
-		{{Form :: text( 'gendertext',$applicant->getGender(), ['readonly']); }}
-		<br>
-		{{Form :: label( 'religion','Religion:') ; }}
-		{{Form :: text( 'religiontext',$applicant->getReligion(), ['readonly']); }}
-		<br>
-		{{Form :: label( 'dob','Date of birth:') ; }}
-		{{Form :: text( 'dobtext',$applicant->getDateOfBirth(), ['readonly']); }}
-		<br>
+						<div id="catogory6" >
+							<div class="form-group">
+								{{Form::label('Label6', 'a) Date returned to country ')}}
+								{{Form:: text( 'dateOfReturned',$category->getDateOfReturned(),array('class' =>'form-control','style'=>'width: 150px; background-color:white','readonly'))  }}		
+							</div>
+							<div class="form-group">
+								{{Form::label('Label7','b) Period of stay abroad')}}
+								{{Form::text( 'PeriodOfStayAbroad',$category->getPerioadAbroadStay()." years",array('class' =>'form-control','style'=>'width: 150px; background-color:white','readonly')) }}	
 
-		{{Form :: label( 'orderOfPreference','This school is applied as :') ; }}
-		{{Form :: text( 'orderOfPreferencetext',$application->getOrderOfPreference(), ['readonly']) ; }}
-		<br>
-		{{Form :: label( 'typeOfApplication','Applied under category:') ; }}
-		@if($application->getType()==1)
-			{{Form :: text( 'typeOfApplicationtext','Resident in closed proximity', ['readonly']) ; }}
-		@elseif($application->getType()==2)
-			{{Form :: text( 'typeOfApplicationtext','Past Pupil of the School', ['readonly']) ; }}
-		@elseif($application->getType()==3)
-			{{Form :: text( 'typeOfApplicationtext','A sibling is learing currenly in school', ['readonly']) ; }}
-		@elseif($application->getType()==4)
-			{{Form :: text( 'typeOfApplicationtext','Educational officer', ['readonly']) ; }}
-		@elseif($application->getType()==5)
-			{{Form :: text( 'typeOfApplicationtext','An officer on transfer', ['readonly']) ; }}
-		@elseif($application->getType()==6)
-			{{Form :: text( 'typeOfApplicationtext','A person coming from abroad', ['readonly']) ; }}
-		@endif
-
-		<br>
-		{{Form :: label( 'distance','Distance to school:') ; }} 
-		{{Form :: text( 'distancetext',$application->getDistance()." km", ['readonly']) ; }} 
-		<br>
-		{{Form :: label( 'medium','Learning Medium requested:') ; }}
-		{{Form :: text( 'mediumtext',$application->getMedium(), ['readonly']) ; }}
-
-
-		<div id="catogory6" >
-			
-			<h1>Childern of persons arriving after living abroad with child</h1>
-
-		{{Form::label('Label6', 'a) Date returned to country ');}}
-
-		 {{ Form :: text( 'dateOfReturned',$category->getDateOfReturned(), ['readonly']) ; }}		
-
-
-		<br>		
-		{{Form::label('Label7','b) Period of stay abroad');}}
-		{{ Form :: text( 'PeriodOfStayAbroad',$category->getPerioadAbroadStay()." years", ['readonly']) ; }}		
-			
-
-		<br>	
-
-		{{Form::label('Label8', 'c) Reason for stay :');}}
-		@if($category->getReasonsForStay()==1)
-			{{Form::text('labler1', 'On Sri lankan dutha mandala duty',array('size'=>'30x1'), ['readonly']);}}
-		@elseif($category->getReasonsForStay()==2)
-			{{Form::text('labelr2', 'On Sri lankan goverment duty purpose', ['readonly']);}}
-        @elseif($category->getReasonsForStay()==3)
-        	{{Form::text('labelr3', 'Scholarship ', ['readonly']);}}
-        @elseif($category->getReasonsForStay()==4)
-        	{{Form::text('labelr4', 'Personal Reason', ['readonly']);}}
-        @endif
-
-        <br>
-
-
-			{{Form::submit('Verify Application')}}		
-			
+							</div>
+							<div class="form-group">
+								{{Form::label('Label8', 'c) Reason for stay :');}}
+								@if($category->getReasonsForStay()==1)
+									{{Form::text('labler1', 'On Sri lankan dutha mandala duty',array('class' =>'form-control','style'=>'background-color:white','readonly'))}}
+								@elseif($category->getReasonsForStay()==2)
+									{{Form::text('labelr2', 'On Sri lankan goverment duty purpose', array('class' =>'form-control','style'=>'background-color:white','readonly'))}}
+        						@elseif($category->getReasonsForStay()==3)
+        							{{Form::text('labelr3', 'Scholarship ',array('class' =>'form-control','style'=>'background-color:white','readonly'))}}
+        						@elseif($category->getReasonsForStay()==4)
+        							{{Form::text('labelr4', 'Personal Reason',array('class' =>'form-control','style'=>'background-color:white','readonly'))}}
+       						    @endif
+							</div>
+							<div class="form-group">
+            					{{Form::submit('Verify Application',array('class' => 'btn btn-primary'))}}		
+							</div>
+					</div>
+					{{Form::close()}}
+					{{ Form:: open(array('url' =>'school/cancelverify','method' => 'POST'))}}
+						{{Form::hidden('schoolid',$school->getSchool_id())}}
+						{{Form::submit('Back',array('class' => 'btn btn-primary','style'=>'position:absolute; right:450px; top:1184px ' ))}}
+					{{Form::close()}}
+						
+					</div></div>
+					
+			</div>
 		</div>
+	</div>
+		
+@stop
 
-	{{Form::close()}}
-
-	{{ Form :: open(array('url' =>'school/cancelverify','method' => 'POST' ))}}
-	{{Form::hidden('schoolid',$school->getSchool_id())}}
-	{{Form::submit('Add to pending list')}}
-	{{Form::close()}}
-</body>
-</html>
