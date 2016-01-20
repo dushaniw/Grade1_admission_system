5d59655aa161060e082c59750dea76db437c790c
Dushani <dushaniwellappili@gmail.com>
2016-01-21 00:26:18 +0530

    cat3,4 done

--- grade1_admission/app/views/G1SAS/category3.blade.php
+++ grade1_admission/app/views/G1SAS/category3.blade.php
@@ -1,17 +1,43 @@
-<html>
-<head>
-	<title>category 3</title>
-</head>
-<body>
-	{{ Form :: open(array('url' =>'selectSchool/addcategory','method' => 'POST' ))}}
-		{{ Form :: hidden( 'orderOfPreference',$application->getOrderOfPreference()) ; }}
-		{{ Form :: hidden( 'typeOfApplication',$application->getType()) ; }}
-		{{ Form :: hidden( 'schoolId',$application->getSchool_id()) ; }}
-		{{ Form :: hidden( 'distance',$application->getDistance()) ; }} 
-		{{ Form :: hidden( 'medium',$application->getMedium()) ; }}
+@extends('G1SAS/layouts/normal_user_layout')
+
+@section('content')
+
+<nav class = "navbar navbar-inverse" role = "navigation">
+  <ul class="nav navbar-nav" >
+    <li class> <a href="/userpage/home?username=<?php echo $username ?>">Home</a></li>
+    <li>{{HTML::link('#', 'About')}}  </li>
+    
+    <li> <a href="/child/childrenoverview?username=<?php echo $username ?>">My Children</a></li>
+    <li class="active"><a href = "" class="disabled" style="color: #3366FF" >My Applications </a></li>
+    <li><a href-"#">Search</a></li>
+  </ul>
+    <p class = "navbar-text pull-right">
+         Signed in as <a href = "#" class = "navbar-link"><?php echo $username?></a> | <a href = "#" class = "navbar-link">Sign Out</a> </p>
+</nav>
+
+<div class="row">
+  <div class="col-md-3">
+    <br/>
+    <ul class = "nav nav-pills nav-stacked" role = "navigation">
+      <li><a href = "#">Overview</a></li>
+      <li class = "active"><a href = "" class="disabled" style="color: #3366FF" >Add New Application</a></li>
+    </ul>
+  </div>
+  <div class = "col-md-9">
+                <div class = "panel panel-primary panel-transparent">
+                        <div class ="panel-heading">
+                                <h3 class="panel-title"> <p class="text-center"><span class = "glyphicon glyphicon-edit"></span> Add New Application </p></h3>
+                        </div>
+                        <div class = "panel-body"> <div class = "col-md-10">
+					{{ Form:: open(array('url' =>'selectSchool/addcategory','method' => 'POST' ))}}
+		{{ Form:: hidden( 'orderOfPreference',$application->getOrderOfPreference()) ; }}
+		{{ Form:: hidden( 'typeOfApplication',$application->getType())}}
+		{{ Form:: hidden( 'schoolId',$application->getSchool_id()) ; }}
+		{{ Form:: hidden( 'distance',$application->getDistance()) ; }} 
+		{{ Form:: hidden( 'medium',$application->getMedium()) ; }}
 	
-	{{	Form :: hidden( 'applicantId',$application->getApplicant_id()) ; }}		
-			{{Form::hidden('schoolId1', $schools[0]);}}
+		{{Form:: hidden( 'applicantId',$application->getApplicant_id()) ; }}		
+		{{Form::hidden('schoolId1', $schools[0]);}}
 		{{Form::hidden('schoolId2', $schools[1]);}}
 		{{Form::hidden('schoolId3', $schools[2]);}}
 		{{Form::hidden('schoolId4', $schools[3]);}}
@@ -38,63 +64,125 @@
 
 		{{Form::hidden('guardianNic', $guardianNic)}}
 	<div id="catogory3" >
-		<h1> Brothers/sisters of students who are studying in school at present </h1>
-		a) If applicant's child/children is/are studying in the school
+		<h3><span class = "label label-default"> Brothers/sisters of students who are studying in school at present </span></h3>
 		<br>
+		<div class="form-group">
+				<h4><strong><u>	a) If applicant's child/children is/are studying in the school</u></strong></h4>
+		</div>
+
+
+		<div class="form-group"> 
+			{{Form:: label( 'first_name_label','First Name')  }}
+			{{Form:: text( 'firstName1','',array('class' =>'form-control','style'=>'background-color:white'))  }}
+			<br/>
+
+			{{Form:: label( 'first_name_label','Last Name')  }}
+			{{Form:: text( 'lastName1','',array('class' =>'form-control','style'=>'background-color:white'))  }}
+			<br/>
+
+			{{ Form:: label( 'admission_number_label','Admission number')  }}
+			{{ Form:: text( 'admissionNumber1','',array('class' =>'form-control','style'=>'width: 150px; background-color:white') )  }}
+			<br/>					
+									
+			{{Form:: label( 'gradeAdmision','Admission grade to the school')  }}
+			{{Form:: text( 'gradeAdmision1','',array('class' =>'form-control','style'=>'width:150px; background-color:white')) }}		
+			<br/>
+
+			{{Form:: label( 'dateOfAdmision','Date of Admission:')  }}	
+			{{Form:: text( 'dateOfAdmision1','',array('class' =>'form-control','style'=>'width:150px; background-color:white'))}}
+			<br/>
+									
+			{{Form:: label( 'ach_label','Educational Achivements:') }}
+			
+			{{Form:: textarea( 'eAchiement1','',array('class' =>'form-control','style'=>'background-color:white')) }}
+			<br/>	 					
+				 					
+			{{Form:: label( 'don_label','Achievements gained for the school by brothers/sisters in the school and various types of assistance provided by the applicant for the development of the school') }}
+			{{Form:: textarea( 'donationDetail1','',array('class' =>'form-control','style'=>'background-color:white')) }}
+			<br/>	 				
+		</div>	
+
+
+		<div class="form-group"> 
+			{{Form:: label( 'first_name_label','First Name')  }}
+			{{Form:: text( 'firstName2','',array('class' =>'form-control','style'=>'background-color:white'))  }}
+			<br/>
+
+			{{Form:: label( 'first_name_label','Last Name')  }}
+			{{Form:: text( 'lastName2','',array('class' =>'form-control','style'=>'background-color:white'))  }}
+			<br/>
+
+			{{ Form:: label( 'admission_number_label','Admission number')  }}
+			{{ Form:: text( 'admissionNumber2','',array('class' =>'form-control','style'=>'width: 150px; background-color:white') )  }}
+			<br/>					
+									
+			{{Form:: label( 'gradeAdmision','Admission grade to the school')  }}
+			{{Form:: text( 'gradeAdmision2','',array('class' =>'form-control','style'=>'width:150px; background-color:white')) }}		
+			<br/>
+									
+			{{Form:: label( 'dateOfAdmision','Date of Admission:')  }}	
+			{{Form:: text( 'dateOfAdmision2','',array('class' =>'form-control','style'=>'width:150px; background-color:white'))}}
+			<br/>
+									
+			{{Form:: label( 'ach_label','Educational Achivements:') }}
+			
+			{{Form:: textarea( 'eAchiement2','',array('class' =>'form-control','style'=>'background-color:white')) }}
+			<br/>	 					
+				 					
+			{{Form:: label( 'don_label','Achievements gained for the school by brothers/sisters in the school and various types of assistance provided by the applicant for the development of the school') }}
+			{{Form:: textarea( 'donationDetail2','',array('class' =>'form-control','style'=>'background-color:white')) }}
+			<br/>	 				
+		</div>	
+
+
+		<div class="form-group"> 
+			{{Form:: label( 'first_name_label','First Name')  }}
+			{{Form:: text( 'firstName3','',array('class' =>'form-control','style'=>'background-color:white'))  }}
+			<br/>
+
+			{{Form:: label( 'first_name_label','Last Name')  }}
+			{{Form:: text( 'lastName3','',array('class' =>'form-control','style'=>'background-color:white'))  }}
+			<br/>
+
+			{{ Form:: label( 'admission_number_label','Admission number')  }}
+			{{ Form:: text( 'admissionNumber3','',array('class' =>'form-control','style'=>'width: 150px; background-color:white') )  }}
+			<br/>					
+									
+			{{Form:: label( 'gradeAdmision','Admission grade to the school')  }}
+			{{Form:: text( 'gradeAdmision3','',array('class' =>'form-control','style'=>'width:150px; background-color:white')) }}		
+			<br/>
+									
+			{{Form:: label( 'dateOfAdmision','Date of Admission:')  }}	
+			{{Form:: text( 'dateOfAdmision3','',array('class' =>'form-control','style'=>'width:150px; background-color:white'))}}
+			<br/>
+									
+			{{Form:: label( 'ach_label','Educational Achivements:') }}
+			
+			{{Form:: textarea( 'eAchiement3','',array('class' =>'form-control','style'=>'background-color:white')) }}
+			<br/>	 					
+				 					
+			{{Form:: label( 'don_label','Achievements gained for the school by brothers/sisters in the school and various types of assistance provided by the applicant for the development of the school') }}
+			{{Form:: textarea( 'donationDetail3','',array('class' =>'form-control','style'=>'background-color:white')) }}
+			<br/>	 				
+		</div>	
+
+		<div class="form-group">
+            				{{Form::submit('Submit',array('class' => 'btn btn-primary'))}}		
+			</div>	
+			{{Form::close()}}
+</div>
+
+
+</div></div>
+                
+       </div>
+  </div>
+</div>
+	@stop
+ 
 
 
 
-		<table border="1" cellpadding="5" cellspacing="5">
-			<tr>
-			<th>First Name</th>
-			<th>Last Name</th>
-			<th>Admission no</th>
-			<th>Admission grade to the school</th>
-			<th>Date of admission</th>
-			<th>Donation done by current students</th>
-			<th>Achievements gained for the school by brothers/sisters in the school and various types of assistance provided by the applicant for the development of the school</th>	
-
-			</tr>
-			<tr>
-
-				<td>		 {{ Form :: text( 'firstName1','') ; }}		</td>
-				<td>		 {{ Form :: text( 'lastName1','') ; }}		</td>	
-				<td>		 {{ Form :: text( 'admissionNumber1','') ; }}		</td>
-				<td>		 {{ Form :: text( 'gradeAdmision1','') ; }}		</td>
-				<td>		 {{ Form :: text( 'dateOfAdmision1','') ; }}		</td>
-				<td>		 {{ Form :: text( 'donationDetail1','') ; }}</td>
-				<td> 		 {{	 Form :: text( 'eAchiement1','') ; }}</td>	
-			</tr>
-			<tr>
-
-			<td>		 {{ Form :: text( 'firstName2','') ; }}		</td>
-				<td>		 {{ Form :: text( 'lastName2','') ; }}		</td>	
-				<td>		 {{ Form :: text( 'admissionNumber2','') ; }}		</td>
-				<td>		 {{ Form :: text( 'gradeAdmision2','') ; }}		</td>
-				<td>		 {{ Form :: text( 'dateOfAdmision2','') ; }}		</td>
-				<td>		 {{ Form :: text( 'donationDetail2','') ; }}</td>
-				<td> 		 {{	 Form :: text( 'eAchiement2','') ; }}</td>
-				</tr>
-			<tr>
-
-			<td>		 {{ Form :: text( 'firstName3','') ; }}		</td>
-				<td>		 {{ Form :: text( 'lastName3','') ; }}		</td>	
-				<td>		 {{ Form :: text( 'admissionNumber3','') ; }}		</td>
-				<td>		 {{ Form :: text( 'gradeAdmision3','') ; }}		</td>
-				<td>		 {{ Form :: text( 'dateOfAdmision3','') ; }}		</td>
-				<td>		 {{ Form :: text( 'donationDetail3','') ; }}</td>
-				<td> 		 {{	 Form :: text( 'eAchiement3','') ; }}</td>
-				</tr>
 
 
-		</table>
-				
-		
-	</div>
-	{{Form :: submit('addApplication')}}
-
-{{Form::close()}}
-
-
-</body>
-</html>
+  
--- grade1_admission/app/views/G1SAS/category4.blade.php
+++ grade1_admission/app/views/G1SAS/category4.blade.php
@@ -1,15 +1,42 @@
-<html>
-<head>
-	<title>category 4</title>
-</head>
-<body>
-{{ Form :: open(array('url' =>'selectSchool/addcategory','method' => 'POST' ))}}
-		{{ Form :: hidden( 'orderOfPreference',$application->getOrderOfPreference()) ; }}
-		{{ Form :: hidden( 'typeOfApplication',$application->getType()) ; }}
-		{{ Form :: hidden( 'schoolId',$application->getSchool_id()) ; }}
-		{{ Form :: hidden( 'distance',$application->getDistance()) ; }} 
-		{{ Form :: hidden( 'medium',$application->getMedium()) ; }}
-		{{	Form :: hidden( 'applicantId',$application->getApplicant_id()) ; }}		
+@extends('G1SAS/layouts/normal_user_layout')
+
+@section('content')
+
+<nav class = "navbar navbar-inverse" role = "navigation">
+  <ul class="nav navbar-nav" >
+    <li class> <a href="/userpage/home?username=<?php echo $username ?>">Home</a></li>
+    <li>{{HTML::link('#', 'About')}}  </li>
+    
+    <li> <a href="/child/childrenoverview?username=<?php echo $username ?>">My Children</a></li>
+    <li class="active"><a href = "" class="disabled" style="color: #3366FF" >My Applications </a></li>
+    <li><a href-"#">Search</a></li>
+  </ul>
+    <p class = "navbar-text pull-right">
+         Signed in as <a href = "#" class = "navbar-link"><?php echo $username?></a> | <a href = "#" class = "navbar-link">Sign Out</a> </p>
+</nav>
+
+<div class="row">
+  <div class="col-md-3">
+    <br/>
+    <ul class = "nav nav-pills nav-stacked" role = "navigation">
+      <li><a href = "#">Overview</a></li>
+      <li class = "active"><a href = "" class="disabled" style="color: #3366FF" >Add New Application</a></li>
+    </ul>
+  </div>
+  <div class = "col-md-9">
+                <div class = "panel panel-primary panel-transparent">
+                        <div class ="panel-heading">
+                                <h3 class="panel-title"> <p class="text-center"><span class = "glyphicon glyphicon-edit"></span> Add New Application </p></h3>
+                        </div>
+                        <div class = "panel-body"> <div class = "col-md-10">
+
+							{{Form:: open(array('url' =>'selectSchool/addcategory','method' => 'POST' ))}}
+		{{ Form:: hidden( 'orderOfPreference',$application->getOrderOfPreference()) ; }}
+		{{ Form:: hidden( 'typeOfApplication',$application->getType()) ; }}
+		{{ Form:: hidden( 'schoolId',$application->getSchool_id()) ; }}
+		{{ Form:: hidden( 'distance',$application->getDistance()) ; }} 
+		{{ Form:: hidden( 'medium',$application->getMedium()) ; }}
+		{{	Form:: hidden( 'applicantId',$application->getApplicant_id()) ; }}		
 		{{Form::hidden('schoolId1', $schools[0]);}}
 		{{Form::hidden('schoolId2', $schools[1]);}}
 		{{Form::hidden('schoolId3', $schools[2]);}}
@@ -37,69 +64,76 @@
 
 		{{Form::hidden('guardianNic', $guardianNic)}}	
 	<div id="catogory4" >
-		<h1> Childern of persons belonging to staff in institution directly involved in school education </h1>
+		<h3><span class = "label label-default"> Childern of persons belonging to staff in institution directly involved in school education </span></h3>
+			<br/>
+
+		<div class="form-group">
+					{{Form:: label( 'medium','a) Post held as permanent employee in the relevant institutions')  }}
+					{{ Form:: text( 'permenentEmployeePost','',array('class' =>'form-control','style'=>'width: 300px; background-color:white') )  }}	
+		</div>
+
+		<div class="form-group">
+			{{Form::label('Label31', 'b) Distance from place of residence to place of work')}}
+			{{Form:: text( 'distanceFromResidentToWork','', array('class' =>'form-control','style'=>'width: 100px; background-color:white'))  }}	
+		</div>
+		<div class="form-group">
+			{{Form::label('Label32','c) Period of difficult school service')}}
+			{{Form:: text( 'nowInDifficultSchoolService','',array('class' =>'form-control','style'=>'width: 100px; background-color:white'))}}		
+		</div>
+		<div class="form-group">
+			{{Form::label('Label33','Difficult school service')}}		
+			{{Form:: text( 'nowInDifficultSchoolService','',  array('class' =>'form-control','style'=>'width: 100px; background-color:white','readonly'))  }}
+							
+		</div>
 		
-		<br>
-		{{Form::label('Label29', 'a) Post held as permanent employee in the relevant institutions');}}
-
-		 {{ Form :: text( 'permenentEmployeePost','') ; }}		
-
-
-		<br>
-		{{Form::label('Label30','Period of service');}}
-
-		 {{ Form :: text( 'totalServicePeriod','') ; }}		
-
-		<br>
-		{{Form::label('Label31', 'b) Distance from place of residence to place of work');}}
-		 {{ Form :: text( 'distanceFromResidentToWork','') ; }}		
-
-
-		<br>
-		{{Form::label('Label32','c) If serving presently in a difficult school, period of difficult school service');}}
-		 {{ Form :: text( 'nowInDifficultSchoolService','') ; }}		
-
-		
-		<br>
-		{{Form::label('Label33','d) If served earlier in a in a difficult school such period of service');}}		
-		 {{ Form :: text( 'nowInDifficultSchoolService','') ; }}		
-		
-		 <br>
-
-		{{Form::label('Label40','Period of service');}}		
-		 {{ Form :: text( 'servingPeriodOfSchool','') ; }}
-		
-
-		<br>
-
-		{{Form::label('Label41','Close School Count ');}}		
-		 {{ Form :: text( 'closeSchoolCount','') ; }}
-		
-
-		<br>
-
-
-		Un-utilized leaves
-		<table border="1" cellpadding="5" cellspacing="5">
-			<tr>
-				<th>2015</th>
-				<th>2014</th>
-				<th>2013</th>
-				<th>2012</th>
-			</tr>
-			<tr>
-				<td>		 {{ Form :: text( 'year1RemLeave','') ; }}		</td>
-				<td>		 {{ Form :: text( 'year2RemLeave','') ; }}		</td>
-				<td>		 {{ Form :: text( 'year3RemLeave','') ; }}		</td>	
-				<td>		 {{ Form :: text( 'year4RemLeave','') ; }}		</td>
-			<tr>
-		</table>
-				 
+		<div class="form-group">
+			{{Form::label('Label40','Serving Period of schools')}}		
+			{{Form:: text( 'servingPeriodOfSchool','', array('class' =>'form-control','style'=>'width: 100px; background-color:white'))  }}
+		</div>
+		<div class="form-group">
+			{{Form::label('Label41','Close School Count ')}}		
+			{{Form:: text( 'closeSchoolCount','',array('class' =>'form-control','style'=>'width: 100px; background-color:white')) }}
+		</div>
+		<div class="form-group">
+			<u><strong>Un-utilized leaves</strong></u>
+		</div>
+		<div class="form-group">
+			{{ Form:: label( 'year1RemLeave','Year 1')  }}
+			{{ Form:: text( 'year1RemLeave','', array('class' =>'form-control','style'=>'width: 100px; background-color:white')) }}
+		</div>
+		<div class="form-group">
+			{{ Form:: label( 'year2RemLeave','Year 2')  }}	
+			{{ Form:: text( 'year2RemLeave','',array('class' =>'form-control','style'=>'width: 100px; background-color:white')) }}		
+							
+		</div>
+		<div class="form-group">
+			{{Form:: label( 'year3RemLeave','Year 3') }}		
+			{{Form:: text( 'year3RemLeave','', array('class' =>'form-control','style'=>'width: 100px; background-color:white')) }}	
+							
+		</div>
+		<div class="form-group">
+			{{ Form:: label( 'year4RemLeave','Year 4') }}			
+			{{ Form:: text( 'year4RemLeave','', array('class' =>'form-control','style'=>'width: 100px; background-color:white'))  }}
+				
+		</div>
+		<div class="form-group">
+         	{{Form::submit('Submit',array('class' => 'btn btn-primary'))}}		
+		</div>
 
 	</div>
 
-	{{Form::submit('addApplication')}}
 
 {{Form::close()}}
-</body>
-</html>
+			
+</div></div>
+                </div>
+  </div>
+</div>
+	@stop
+ 
+
+
+
+
+
+  
